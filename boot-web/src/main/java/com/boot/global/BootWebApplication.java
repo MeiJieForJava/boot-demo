366435ea603e1d2199ce4ae42b23aaeb79bff346
meijie3 <meijie@jd.com>
2019-01-17 11:46:05 +0800

    commit

--- boot-web/src/main/java/com/boot/global/BootWebApplication.java
+++ boot-web/src/main/java/com/boot/global/BootWebApplication.java
@@ -13,3 +13,33 @@ public class BootWebApplication {
         SpringApplication.run(BootWebApplication.class, args);
     }
 }
+
+
+
+
+//使用外置tomcat
+
+//package com.boot.global;
+//
+//        import org.springframework.boot.SpringApplication;
+//        import org.springframework.boot.autoconfigure.SpringBootApplication;
+//        import org.springframework.boot.builder.SpringApplicationBuilder;
+//        import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//        import org.springframework.context.annotation.ComponentScan;
+//        import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//@SpringBootApplication
+//@ComponentScan(basePackages = "com.boot")
+//@EnableSwagger2
+//public class BootWebApplication extends SpringBootServletInitializer {
+////public class BootWebApplication implements WebMvcConfigurer {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(BootWebApplication.class, args);
+//    }
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(BootWebApplication.class);
+//    }
+//}
